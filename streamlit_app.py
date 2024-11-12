66bf67b97cba2ff9b2b9fadea617e73f637d5dcf
Haithem1999 <41543751+Haithem1999@users.noreply.github.com>
2024-11-12 13:46:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -133,7 +133,7 @@ def generate_response(prompt):
 
     response = client.chat.completions.create(
         #model="ft:gpt-4o-mini-2024-07-18:personal::AHYHvnQx",
-        model = "ft:gpt-4o-mini-2024-07-18:personal::AHYHvnQx",
+        model = "gpt-4o-mini",
         messages=[{"role": "system", "content": system_prompt}] + st.session_state.messages + [{"role": "user", "content": user_prompt}],
     )
     
